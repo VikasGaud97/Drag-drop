b077a8912259c992a1317a9165241a6da88567f6
vikasgaud95@gmail.com <vikasgaud95@gmail.com>
2023-06-15 22:24:04 +0530

    html,css and js

--- main.js
+++ main.js
@@ -1,27 +1,8 @@
 let lists = document.getElementsByClassName(".list")
-// let right_box = document.getElementById("right")
-// let left_box = document.getElementById("left")
-
-// for (let lists of divChild) {
-//   lists.addEventListener("dragstart", function(e){
-//     let selected = e.target;
-//     right_box.addEventListener("dragover",function(e){
-//       e.preventDefault();
-//       // console.log(dragging)
-//     })
-//      right_box.addEventListener("drop",function(e){
-//       right_box.appendChild(divChild);
-//       selected = null;
-//     })
-//   })
-// }
-
+// creating function to child node in to parent node
 const parentNode = document.getElementById("left") //parent node
 let right_box = document.getElementById("right");
-
-
 const tasks = ["Task 1", "Task 2", "Task 3", "Task 4"];
-// creating function to child node in to parent node
 
 for (let task_item of tasks) {
   let paraChild1 = document.createElement("div");
@@ -29,6 +10,7 @@ for (let task_item of tasks) {
   paraChild1.draggable = "true"
   paraChild1.innerHTML = `<img src="icon/drag.png">${task_item}`;
   parentNode.appendChild(paraChild1);
+  //drag function
   paraChild1.addEventListener("dragstart", function(e) {
     let paraChild2 = document.createElement("div")
     paraChild2 = e.target;
@@ -42,7 +24,7 @@ for (let task_item of tasks) {
   });
 
 }
-
+//reset button function
 function reset() {
   var container;
   location.reload(true) = function() {
